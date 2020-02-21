536f37226bccad817269c4987f4b14d52c6761fb
Deidre <deidre.balard@outlook.com>
2020-02-20 19:18:47 -0500

    commit

---
+++ Chapter 4/Chapter 4 pag 52/Chapter 4 pag 52/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_4_pag_52
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer:");
+            int myInt = Convert.ToInt32(Console.ReadLine());
+            bool isLessThan10 = myInt < 10;
+            bool isBetween0ANd5 = (0 <= myInt) && (myInt <= 5);
+            Console.WriteLine("Integer less than 10? {0}", isLessThan10);
+            Console.WriteLine("Integer Between 0 and 5? {0}", isBetween0ANd5);
+            Console.WriteLine("Exactly one of the above is true? {0}",
+                isLessThan10 ^ isBetween0ANd5);
+            Console.ReadKey();
+        }
+
+
+
+
+        }
+    }
+                          
+
---
+++ Chapter 4/Chapter 4 pag 71/Chapter 4 pag 71/Chapter 4 pag 71/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_4_pag_71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double realCoord, imagCoord;
+            double realTemp, imagTemp, realTemp2, arg;
+            int interations;
+            for (imagCoord = 1.2; imagCoord >= -1.2; imagCoord -= 0.05)
+            {
+                for (realCoord = -0.6; realCoord <= 1.77; realCoord += 0.03)
+                {
+                    interations = 0;
+                    realTemp = realCoord;
+                    imagTemp = imagCoord;
+                    arg = (realCoord * realCoord) + (imagCoord * imagCoord);
+                    while ((arg < 4) && (interations < 40))
+                    {
+                        realTemp2 = (realTemp * realTemp) - (imagTemp * imagTemp)
+                            - realCoord;
+                        imagTemp = (2 * realTemp * imagTemp) - imagCoord;
+                        realTemp = realTemp2;
+                        arg = (realTemp * realTemp) + (imagTemp * imagTemp * imagTemp);
+                        interations += 1;
+                    }
+                    switch (interations % 4)
+                    {
+                        case 0:
+                            Console.Write(".");
+                            break;
+                        case 1:
+                            Console.Write("o");
+                            break;
+                        case 2:
+                            Console.Write("0");
+                            break;
+                        case 3:
+                            Console.Write("@");
+                            break;
+                    }
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+                                    }
+        
+    
+}
